4eeb7489c6452aa365b8ceb08e969204e5ec0782
AlexJediVoin <studying.info.tech@gmail.com>
2022-01-13 18:44:13 +0500

    Added SuperButton componenta.

---
+++ src/SuperButton/SuperButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './SuperButton.css';
+
+type SuperButtonPropsType = {
+    title: string
+    disabled: boolean
+    onClickBtn: () => void
+}
+
+
+function SuperButton(props: SuperButtonPropsType) {
+
+    const OnClickHandler = () => {
+        if (props.title === "Set") {
+            props.onClickBtn();
+        }
+        if (props.title === "inc") {
+            props.onClickBtn();
+        }
+        if (props.title === "reset") {
+            props.onClickBtn();
+        }
+    }
+
+    return (
+        <div className="SuperButton">
+            <button disabled={props.disabled} onClick={OnClickHandler}>{props.title}</button>
+        </div>
+    );
+}
+
+export default SuperButton;
